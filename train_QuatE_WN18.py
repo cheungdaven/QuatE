a77aef81489842fa2fadb89be785b25483d1b44c
Shuai Zhang <cheungdaven@gmail.com>
2019-11-01 12:45:09 +0800

    Update train_QuatE_WN18.py

--- train_QuatE_WN18.py
+++ train_QuatE_WN18.py
@@ -1,4 +1,31 @@
-
+import config
+from  models import *
+import json
+con = config.Config()
+con.set_in_path("./benchmarks/WN18/")
+con.set_work_threads(8)
+con.set_train_times(3000)
+con.set_nbatches(10)
+con.set_alpha(0.1)
+con.set_bern(1)
+con.set_dimension(300)
+con.set_lmbda(0.05)
+con.set_lmbda_two(0.05)
+con.set_margin(1.0)
+con.set_ent_neg_rate(10)
+con.set_rel_neg_rate(0)
+con.set_opt_method("adagrad")
+con.set_save_steps(500)
+con.set_valid_steps(500)
+con.set_early_stopping_patience(10)
+con.set_checkpoint_dir("./checkpoint")
+con.set_result_dir("./result")
+con.set_test_link(True)
+con.set_test_triple(True)
+con.init()
+con.set_train_model(QuatE)
+con.train()
+'''
 import config
 from  models import *
 import json
@@ -25,3 +52,4 @@ con.set_test_triple(True)
 con.init()
 con.set_train_model(QuatE)
 con.train()
+'''
